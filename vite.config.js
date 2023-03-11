09c5402fe96923dc5b679e5e5d2837f4fd5b59e7
Sahil Khan <96828919+S-Khan786@users.noreply.github.com>
2023-03-11 18:20:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/https://github.com/S-Khan786/todolist-app.github.io/'
   plugins: [vue()],
 })
